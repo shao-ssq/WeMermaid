c1d491c1142e8d2ae84027b9f41a6f488366619e
燕双嘤 <1966799809@qq.com>
2025-09-29 17:25:22 +0800

    简单修改

--- app/layout.js
+++ app/layout.js
@@ -1,33 +1,19 @@
-import { Geist, Geist_Mono } from "next/font/google";
-import { Toaster } from "@/components/ui/sonner";
-import { ThemeProvider } from "@/components/theme-provider";
+import {ThemeProvider} from "@/components/theme-provider";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 export const metadata = {
-  title: "WeMermaid",
-  description: "利用 AI 技术将文本内容智能转换为 Mermaid 格式的可视化图表",
+    title: "WeMermaid",
+    description: "利用 AI 技术将文本内容智能转换为 Mermaid 格式的可视化图表",
 };
 
-export default function RootLayout({ children }) {
-  return (
-    <html lang="zh-CN" suppressHydrationWarning>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+export default function RootLayout({children}) {
+    return (
+        <html lang="zh-CN" suppressHydrationWarning>
+        <body>
         <ThemeProvider defaultTheme="system" storageKey="theme">
-          {children}
+            {children}
         </ThemeProvider>
-      </body>
-    </html>
-  );
+        </body>
+        </html>
+    );
 }
